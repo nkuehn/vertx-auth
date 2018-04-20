0ee31ce389124524a6bbcc1c9038361c32ba1212
Nikolaus Kühn <nikolaus.kuehn@commercetools.de>
2018-04-21 00:15:04 +0200

    set back keycloak provider to original implementation

--- vertx-auth-oauth2/src/main/java/io/vertx/ext/auth/oauth2/providers/KeycloakAuth.java
+++ vertx-auth-oauth2/src/main/java/io/vertx/ext/auth/oauth2/providers/KeycloakAuth.java
@@ -91,9 +91,9 @@ public interface KeycloakAuth {
       options.addPubSecKey(new PubSecKeyOptions()
         .setAlgorithm("RS256")
         .setPublicKey(config.getString("realm-public-key")));
+      // we could load keys
+      options.setJWTToken(true);
     }
-    // keycloak uses JWT
-    options.setJWTToken(true);
 
     return OAuth2Auth.create(vertx, flow, options);
   }
